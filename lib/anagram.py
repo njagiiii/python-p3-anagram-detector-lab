0b550f564d3b8fa0e9760e66dba9c434954689eb
njagiiii <grace.makena@student.moringaschool.com>
2023-08-26 15:00:51 -0400

    completed assignments

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self.word =word.lower()
+
+    def match(self,words):
+        anagrams = []
+        for word in words:
+            if sorted(self.word) == sorted(word.lower()) and self.word != word.lower():
+                anagrams.append(word)
+        return anagrams
